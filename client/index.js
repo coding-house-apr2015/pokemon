f0e4766a0428ec14f53ff98c28825993290d35d1
Chyld Medford <chyld.medford@gmail.com>
2015-04-14 16:55:35 -0700

    fin

--- client/index.js
+++ client/index.js
@@ -6,8 +6,65 @@
 $(document).ready(init);
 
 function init(){
-  $('#pokedex').on('click', '.pokemon', getPokemon);
+  $('#pokedex').on('click', '.pokemon:not(.filled)', getPokemon);
+  $('#pokedex').on('click', '.filled', choosePokemon);
+  $('#clear').click(clearPokemon);
+  $('#fight').click(fight);
   drawPokedex();
+}
+
+function fight(){
+  var $p1 = $('.player1');
+  var $p2 = $('.player2');
+  var p1 = {};
+  var p2 = {};
+
+  p1.attack = $('.player1').find('li:nth-child(1)').text().split(':')[1] * 1;
+  p1.defense = $('.player1').find('li:nth-child(2)').text().split(':')[1] * 1;
+  p1.exp = $('.player1').find('li:nth-child(3)').text().split(':')[1] * 1;
+  p1.hp = $('.player1').find('li:nth-child(4)').text().split(':')[1] * 1;
+
+  p2.attack = $('.player2').find('li:nth-child(1)').text().split(':')[1] * 1;
+  p2.defense = $('.player2').find('li:nth-child(2)').text().split(':')[1] * 1;
+  p2.exp = $('.player2').find('li:nth-child(3)').text().split(':')[1] * 1;
+  p2.hp = $('.player2').find('li:nth-child(4)').text().split(':')[1] * 1;
+
+  hit(p1, p2);
+  hit(p2, p1);
+
+  if(p1.hp <= 0){
+    $p1.remove();
+  }
+
+  if(p2.hp <= 0){
+    $p2.remove();
+  }
+
+  $('.player1').find('li:nth-child(4)').text('hp:' + p1.hp);
+  $('.player2').find('li:nth-child(4)').text('hp:' + p2.hp);
+}
+
+function hit(p1, p2){
+  var attack = p1.attack * (p1.exp / 100);
+  var defense = p2.defense * (p2.exp / 100);
+  var final = Math.abs(attack - defense);
+  var random = Math.floor(Math.random() * final);
+  p2.hp -= random;
+}
+
+function choosePokemon(){
+  var $self = $(this);
+  if($self.is('.player1')){
+    $self.removeClass('player1').addClass('player2');
+  }else if($self.is('.player2')){
+    $self.removeClass('player2');
+  }else{
+    $self.addClass('player1');
+  }
+}
+
+function clearPokemon(){
+  $('.pokemon:not(.filled)').remove();
 }
 
 function getPokemon(){
@@ -17,14 +74,13 @@ function getPokemon(){
   var url = domain + uri;
   $.getJSON(url, function(response){
 
-    console.log(response);
-
+    $self.addClass('filled');
     var $ul = $self.find('ul');
     var attributes = ['attack', 'defense', 'exp', 'hp'];
     var $lis = attributes.map(function(attribute){
       return '<li>' + attribute + ':' + response[attribute] + '</li>'
     });
-    $ul.append($lis);
+    $ul.empty().append($lis);
 
     var spriteUrls = response.sprites.map(function(o){
       return domain + o.resource_uri;
