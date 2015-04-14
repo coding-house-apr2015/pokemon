f71b4bce943fd2f5061721cab46796c349fcc68a
Chyld Medford <chyld.medford@gmail.com>
2015-04-14 11:15:59 -0700

    loop

--- client/index.js
+++ client/index.js
@@ -1,1 +1,25 @@
+/* global pokedex:true */
+/* jshint camelcase:false */
+
 'use strict';
+
+$(document).ready(init);
+
+function init(){
+  drawPokedex();
+}
+
+function drawPokedex(){
+  pokedex.pokemon.forEach(function(pokemon){
+    var $outer = $('<div>');
+    var $inner = $('<div>');
+
+    $outer.addClass('pokemon');
+    $outer.attr('data-uri', pokemon.resource_uri);
+    $inner.addClass('name');
+    $inner.text(pokemon.name);
+
+    $outer.append($inner);
+    $('#pokedex').append($outer);
+  });
+}
